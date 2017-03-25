33c8e148eaa45f0871a38569aa2eea4793e384a9
sunilgudivda369 <sunilgudivada369@gmail.com>
2017-03-25 20:39:51 +0530

    Added cos and tan
    Added cos and tan , check the y=x line in comments

--- main.cpp
+++ main.cpp
@@ -22,6 +22,14 @@ void plotSine() {
 
         glVertex2d(0, 1);
         glVertex2d(0, -1);
+        /*for(int i=0 ; i<100 ; i++){
+            for(int j=0 ; j<100 ; j++){
+                if (i == j ){
+                   glVertex2d(i * scaleFactorX, j * scaleFactorY);
+                   glVertex2d(-i * scaleFactorX, -j * scaleFactorY);
+                }
+            }
+        }*/
 	glEnd();
 
 	glBegin(GL_POINTS);
@@ -40,12 +48,86 @@ void plotSine() {
 	glutPostRedisplay();
 }
 
+void plotCos() {
+    double x = -4*PI, y;
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_LINES);
+        glColor3f(0.5, 0.5, 0.5);
+        glVertex2d(-1, 0);
+        glVertex2d(1, 0);
+
+        glVertex2d(0, 1);
+        glVertex2d(0, -1);
+	glEnd();
+
+	glBegin(GL_POINTS);
+        glColor3f(1, 1, 1);
+        do {
+            y = cos(x);
+            x = x + PRECISION;
+            glVertex2d(x * scaleFactorX, y * scaleFactorY);
+		} while(x<=4*PI);
+	glEnd();
+	glFlush();
+
+	scaleFactorX += ZOOM;
+
+	glutPostRedisplay();
+}
+
+
+void plotTan() {
+    double x = -4*PI, y;
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_LINES);
+        glColor3f(0.5, 0.5, 0.5);
+        glVertex2d(-1, 0);
+        glVertex2d(1, 0);
+
+        glVertex2d(0, 1);
+        glVertex2d(0, -1);
+	glEnd();
+
+	glBegin(GL_POINTS);
+        glColor3f(1, 1, 1);
+        do {
+            y = tan(x);
+            x = x + PRECISION;
+
+            glVertex2d(x * scaleFactorX, y * scaleFactorY);
+		} while(x<=4*PI);
+	glEnd();
+	glFlush();
+
+	scaleFactorX += ZOOM;
+
+	glutPostRedisplay();
+}
+
+void display(){
+    int type;
+    cout<< "Select option : \n 1) SIN() \n 2) COS() \n 3) TAN() \n" ;
+    cin >> type;
+    switch(type) {
+        case 1 :  glutDisplayFunc(plotSine);;
+                  break ;
+        case 2 :  glutDisplayFunc(plotCos);
+                  break ;
+        case 3 :  glutDisplayFunc(plotTan);;
+                  break ;
+   }
+}
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Sine Function Plot");
-	glutDisplayFunc(plotSine);
+	glutCreateWindow("Function Plot");
+	display();
 	glutMainLoop();
 	return 0;
 }
